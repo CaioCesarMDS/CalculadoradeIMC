4a8da2707e94d4aed50a4916337d8eaebe5e63df
Caio Cesar <caiocesarsts@gmail.com>
2023-11-19 17:48:24 -0300

    refactoring code and add toggle green mark

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,9 @@
+document.getElementById("result-div").style.display = "none";
+document.getElementById("triangle").style.display = "none";
+
+document.getElementById("imc-calculator").addEventListener("submit", calculateImc);
+
 function calculateImc(event) {
-
-
-    document.getElementById("result-div").style.display = "block";
-    document.getElementById("triangle").style.display = "block";
-
     // Evita que site se atualize a cada submit;
     event.preventDefault();
 
@@ -16,20 +16,18 @@ function calculateImc(event) {
     }
 
     height = height / 100;
-    let imc = (weight / (height * height))
+    let imc = weight / (height * height);
     imc = imc.toFixed(2);
     document.getElementById("imc").innerHTML = imc;
-    let minWeight = ((height * height) * 18.5).toFixed(1);
-    let maxWeight = ((height * height) * 24.9).toFixed(1);
 
-    // Só atribui o valor de min/maxWeight para 1 ID.
-    // document.getElementById("minWeight").innerHTML = minWeight;
-    // document.getElementById("maxWeight").innerHTML = maxWeight;
+    const minWeight = (height * height * 18.5).toFixed(1);
+    const maxWeight = (height * height * 24.9).toFixed(1);
+    const overWeight = (height * height * 30).toFixed(1);
 
-    let overWeight = ((height * height) * 30).toFixed(1);
-    document.getElementsByClassName("overWeight")[0].innerHTML = overWeight;
-    document.getElementsByClassName("overWeight")[1].innerHTML = overWeight;
+    displayImcResults(imc, minWeight, maxWeight, overWeight);
+}
 
+function displayImcResults(imc, minWeight, maxWeight, overWeight) {
     // Obtendo todos os elementos com a classe "minWeight"
     let minWeightElements = document.getElementsByClassName("minWeight");
 
@@ -41,47 +39,62 @@ function calculateImc(event) {
     // Fazendo o mesmo para a classe "maxWeight"
     let maxWeightElements = document.getElementsByClassName("maxWeight");
 
-    for (let i = 0; i < maxWeightElements.length; i++) {
+    for (i = 0; i < maxWeightElements.length; i++) {
         maxWeightElements[i].innerHTML = maxWeight;
     }
 
+    let overWeightElements = document.getElementsByClassName("overWeight");
+
+    for (i = 0; i < overWeightElements.length; i++) {
+        overWeightElements[i].innerHTML = overWeight;
+    }
+
+    //Como a classe "overWeight" só precisa ser mostrado duas vezes, não é preciso criar um for, é só colocar o número do índice, que é representado pela vez que aparece no HTML, Ex: [0], primeiro vez que a classe "overWeight" aparece, e assim sucessivamente. Assim o valor que você atribuir, será colocada no elemente nessa posição. (Assim o script tem um melhor desempenho)
+
+    // document.getElementsByClassName("overWeight")[0].innerHTML = overWeight;
+    // document.getElementsByClassName("overWeight")[1].innerHTML = overWeight;
+
+    const allElementsTable = document.querySelectorAll(
+        ".lineTableLowImc, .lineTableNormalImc, .lineTableOverImc, .lineTableHighImc"
+    );
+
+    allElementsTable.forEach(function (lineTable) {
+        lineTable.classList.remove("greenMark");
+    });
 
     if (imc < 18.5) {
         document.getElementById("explainImc").innerHTML = "está abaixo do recomendado";
-        document.getElementById("imcGoal").inneHTML = "atingir";
-        let elementsTable = document.querySelectorAll(".lineTableLowImc");
+        document.getElementById("imcGoal").innerHTML = "atingir";
 
-        elementsTable.forEach(function (lineTableLow) {
-            lineTableLow.classList.add("greenMark");
-        })
+        addGreenMark(".lineTableLowImc");
     } else if (imc >= 18.5 && imc <= 24.9) {
         document.getElementById("explainImc").innerHTML = "é considerado normal";
         document.getElementById("imcGoal").innerHTML = "manter";
-        let elementsTable = document.querySelectorAll(".lineTableNormalImc");
 
-        elementsTable.forEach(function (lineTableNormal) {
-            lineTableNormal.classList.add("greenMark");
-        })
+        addGreenMark(".lineTableNormalImc");
     } else if (imc > 24.9 && imc <= 30) {
         document.getElementById("explainImc").innerHTML = "está acima do recomendado";
         document.getElementById("imcGoal").innerHTML = "atingir";
-        let elementsTable = document.querySelectorAll(".lineTableOverImc");
 
-        elementsTable.forEach(function (lineTableOver) {
-            lineTableOver.classList.add("greenMark");
-        })
+        addGreenMark(".lineTableOverImc");
     } else {
         document.getElementById("explainImc").innerHTML = "está acima do recomendado";
         document.getElementById("imcGoal").innerHTML = "atingir";
-        let elementsTable = document.querySelectorAll(".lineTableHighImc");
 
-        elementsTable.forEach(function (lineTableHigh) {
-            lineTableHigh.classList.add("greenMark");
-        })
+        addGreenMark(".lineTableHighImc");
     }
+    showHiddenItens();
 }
 
-document.getElementById("result-div").style.display = "none";
-document.getElementById("triangle").style.display = "none";
+function addGreenMark(selector) {
+    const elementsTable = document.querySelectorAll(selector);
 
-document.getElementById("imc-calculator").addEventListener("submit", calculateImc);
+    elementsTable.forEach(function (lineTable) {
+        lineTable.classList.add("greenMark");
+    });
+}
+
+function showHiddenItens() {
+    document.getElementById("result-div").style.display = "block";
+    document.getElementById("triangle").style.display = "block";
+}
